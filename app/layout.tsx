a1e438fbbe8776ab218529fae0d35ba605801e88
zedaenious <matt@zelenak.me>
2025-06-19 15:06:30 -0400

    root layout; primary dark mode bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Inter } from 'next/font/google'
 
@@ -15,8 +16,14 @@ export const metadata = {
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
+      <body
+        className={`${inter.className} antialiased bg-gradient leading-relaxed text-slate-400 selection:selected-text`}
+      >
+        <div className="relative">
+          <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
