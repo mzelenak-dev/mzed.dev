2d980c779db66b3a7cb44fbc15ac5d2f22132b49
zedaenious <matt@zelenak.me>
2025-09-08 23:05:35 -0400

    prod build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,9 @@ export default function Home() {
       <div className="lg:flex lg:justify-between lg:gap-4">
         <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
           <div>
-            <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl"><a href="/">Matt Zelenak</a></h1>
+            <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+              <Link href="/" >Matt Zelenak</Link>
+            </h1>
             <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Full Stack Engineer</h2>
             <p className="mt-4 lg:max-w-xs max-w-full leading-normal">Building scalable, human-centered applications that bridge business goals with seamless user experiences.</p>
             <nav className="nav hidden lg:block" aria-label="In-page jump links">
--- components/Pills.tsx
+++ components/Pills.tsx
@@ -9,9 +9,9 @@ const Pills = ({list}: PillsProps) => {
   const pills = list.map((item, index) => (
     <li
       key={`item-${index}-${genListKey()}`}
-      className="mr-1.5 mt-2"
+      className="mr-1 mt-2"
     >
-      <div className='flex items-center rounded-xl bg-teal-400/10 px-3 py-2 text-xs md:font-medium text-teal-300'>
+      <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
         {item}
       </div>
     </li>
